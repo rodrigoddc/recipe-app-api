f17bad873bf03bf0cd36f22c6f7c8549ad82c237
rodrigo <rodrigoddc1@gmail.com>
2021-06-13 23:01:57 -0300

    flake8

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
 
     def test_create_user_with_email(self):
@@ -23,10 +24,9 @@ class ModelTests(TestCase):
 
         email = 'qwer@QWER.com'
 
-        user = get_user_model().objects.create_user(email,'qwer1234')
+        user = get_user_model().objects.create_user(email, 'qwer1234')
 
         self.assertEqual(user.email, email.lower())
-
 
     def test_new_user_invalid_email(self):
         """Test creating user with invalid email throws error"""
@@ -37,7 +37,8 @@ class ModelTests(TestCase):
     def test_create_super_user(self):
         """Test creating a new super user"""
 
-        user = get_user_model().objects.create_superuser('qwer@qwer.com', 'qwer1234')
+        user = get_user_model().objects.create_superuser('qwer@qwer.com',
+                                                         'qwer1234')
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
